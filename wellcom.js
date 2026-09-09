5a73e1672250b5841cc0251be343d463e835344f
xaro-2007 <phamvanxaro2007@gmail.com>
2026-09-10 00:37:41 +0700

    Delete wellcom.js

--- wellcom.js
+++
@@ -1,9 +0,0 @@
-
-    //alert("Welcome to my website");
-    console.log("Welcome to my website");
-   
-    let a = 10;
-    let b = 20;
-    let sum = a+b;
-    console.log("The result is : "+ sum);
-    
