654efc5dfaed359b9672fb8273935a58bf5b6d03
Pham Van Xa Ro <phamvanxaro2007@gmail.com>
2026-09-09 23:50:29 +0700

    Table

---
+++ wellcom.js
@@ -0,0 +1,9 @@
+
+    //alert("Welcome to my website");
+    console.log("Welcome to my website");
+   
+    let a = 10;
+    let b = 20;
+    let sum = a+b;
+    console.log("The result is : "+ sum);
+    
